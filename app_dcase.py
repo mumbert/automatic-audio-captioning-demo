55010b0cbf08636a2ac5f0abc12fbeba2c8cd088
Marti Umbert <marti.umbert@gmail.com>
2025-02-08 23:15:38 +0000

    app_dcase.py requirements.txt: go back to simple demo with CLAP twice, place dcase baseline in dedicated branch until installation is solved

--- app_dcase.py
+++ app_dcase.py
@@ -1,40 +1,26 @@
 
-# import gradio as gr
-# from msclap import CLAP
-
-# clap_model = CLAP(version = 'clapcap', use_cuda=False)
-
-# def clap_inference(mic=None, file=None):
-
-#     if mic is not None:
-#         audio = mic
-#     elif file is not None:
-#         audio = file
-#     else:
-#         return "You must either provide a mic recording or a file"
-
-#     # Generate captions for the recording
-#     captions = clap_model.generate_caption([audio], 
-#                                            resample=True, 
-#                                            beam_size=5, 
-#                                            entry_length=67, 
-#                                            temperature=0.01)
-
-#     return captions[0]
-
-from transformers import pipeline
 import gradio as gr
-import torchaudio
-import torch
-from dcase24t6.nn.hub import baseline_pipeline
-import os
-import gdown
+from msclap import CLAP
 
-def download_dcase_model_checkpoint():
+clap_model = CLAP(version = 'clapcap', use_cuda=False)
 
-    url = "https://drive.google.com/uc?id=1JABWIBlHuLAhYPX5ktbyLniH-YpeRyeT"
-    output = 'epoch_232-step_001864-mode_min-val_loss_3.3752.ckpt'
-    gdown.download(url, output, quiet=False)
+def clap_inference(mic=None, file=None):
+
+    if mic is not None:
+        audio = mic
+    elif file is not None:
+        audio = file
+    else:
+        return "You must either provide a mic recording or a file"
+
+    # Generate captions for the recording
+    captions = clap_model.generate_caption([audio], 
+                                           resample=True, 
+                                           beam_size=5, 
+                                           entry_length=67, 
+                                           temperature=0.01)
+
+    return captions[0]
 
 def create_app():
 
@@ -45,7 +31,7 @@ def create_app():
             """
         )
         gr.Interface(
-            fn=dcase_inference,
+            fn=clap_inference,
             inputs=[
                 gr.Audio(sources="microphone", type="filepath"),
                 gr.Audio(sources="upload", type="filepath"),
@@ -54,8 +40,6 @@ def create_app():
         )
 
     return demo
-
-download_dcase_model_checkpoint()
 
 def main():
     
